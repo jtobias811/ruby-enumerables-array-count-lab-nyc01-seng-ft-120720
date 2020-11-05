20fbef198947ea16df4e5a640bb67ac003b6a5b3
Jacob Tobias <jtobias811@gmail.com>
2020-11-05 15:28:29 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,10 +1,11 @@
 def count_strings(array)
   array.count do |strings|
-    strings = String.class
+    strings =
   end
 end
 
 def count_empty_strings(array)
-  array.count do |empty|
+  array.count do |empty_strings|
+    empty_strings = ""
   end
 end
